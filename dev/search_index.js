8538c54d15a09def3a6858252fcd02af9c5501c7
Documenter.jl <documenter@juliadocs.github.io>
2024-05-23 19:58:28 +0000

    build based on 335378e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FETI","category":"page"},{"location":"#FETI","page":"Home","title":"FETI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FETI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FETI]","category":"page"}]
+}
