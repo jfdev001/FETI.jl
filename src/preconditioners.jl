92073a24a2c6c44f7af19dfc56894ac8f0233422
Jared Frazier <cscidev001@gmail.com>
2024-06-03 14:45:33 +0200

    doc for TracePreconditioner from Farhat1991

--- src/preconditioners.jl
+++ src/preconditioners.jl
@@ -6,6 +6,18 @@ abstract type Preconditioner end
 Dirichlet preconditioner that uses a Schur-complement to estimate interface 
 forces. This is the most accurate but also most expensive preconditioner.
 """
-struct DirichletPreconditioner <: Preconditioner
+struct DirichletPreconditioner <: Preconditioner end  
 
-end  
+@doc raw"""
+    struct TracePreconditioner <: Preconditioner
+
+Preconditioner ``P_{I}^{-1} = \sum_{j=1}^{N_s} \mathbf{K}_{j}^{I}`` for 
+computation of interface ``I`` constraints by the preconditioned conjugate 
+projected gradient method for ``N_s`` subdomains with interface dofs of local 
+stiffness matrices ``\mathbf{K}_{j}^{I}``.
+
+# References
+[1] : Equation (24) from Farhat, C. A Method of Finite Element Tearing and 
+Interconnecting and its Parallel Solution Algorithm. vol 32, 1205-1227. 1991.  
+"""
+struct TracePreconditioner <: Preconditioner end
